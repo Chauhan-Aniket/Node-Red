be9b8836abfc5db74fe2e886ef48dd4608a5fba5
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-07-06 11:51:29 +0530

    created express app

--- settings.js
+++ settings.js
@@ -1,100 +1,34 @@
-/**
- * Copyright 2014 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
+var http = require("http");
+var express = require("express");
+var RED = require("node-red");
 
-const path = require("path");
+// Create an Express app
+var app = express();
 
-var settings = (module.exports = {
-	uiPort: process.env.PORT || 1880,
-	mqttReconnectTime: 15000,
-	serialReconnectTime: 15000,
-	debugMaxLength: 10000000,
+// Add a simple route for static content served from 'public'
+app.use("/", express.static("public"));
 
-	// Add the nodes in
-	nodesDir: path.join(__dirname, "nodes"),
+// Create a server
+var server = http.createServer(app);
 
-	// Blacklist the non-bluemix friendly nodes
-	nodesExcludes: [
-		"66-mongodb.js",
-		"75-exec.js",
-		"35-arduino.js",
-		"36-rpi-gpio.js",
-		"25-serial.js",
-		"28-tail.js",
-		"50-file.js",
-		"31-tcpin.js",
-		"32-udp.js",
-		"23-watch.js",
-	],
+// Create the settings object - see default settings.js file for other options
+var settings = {
+	httpAdminRoot: "/red",
+	httpNodeRoot: "/api",
+	userDir: "/home/nol/.nodered/",
+	functionGlobalContext: {}, // enables global context
+};
 
-	// Enable module reinstalls on start-up; this ensures modules installed
-	// post-deploy are restored after a restage
-	autoInstallModules: true,
+// Initialise the runtime with a server and settings
+RED.init(server, settings);
 
-	// Move the admin UI
-	httpAdminRoot: "/editor",
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot, RED.httpAdmin);
 
-	// Move the dashboard UI
-	ui: { path: "/" },
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot, RED.httpNode);
 
-	// Never change flow's file
-	flowFile: "flows.json",
+server.listen(8000);
 
-	// You can protect the user interface with a userid and password by using the following property
-	// the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
-	//httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
-
-	// Serve up the welcome page
-	httpStatic: path.join(__dirname, "public"),
-
-	functionGlobalContext: {},
-
-	httpNodeCors: {
-		origin: "*",
-		methods: "GET,PUT,POST,DELETE",
-	},
-
-	// Disbled Credential Secret
-	credentialSecret: false,
-
-	editorTheme: {
-		projects: {
-			enabled: false,
-		},
-	},
-});
-
-if (process.env.NODE_RED_a_USERNAME && process.env.NODE_RED_b_PASSWORD) {
-	settings.adminAuth = {
-		type: "credentials",
-		users: function (username, resolve) {
-			if (process.env.NODE_RED_a_USERNAME == username) {
-				return resolve({ username: username, permissions: "*" });
-			} else {
-				return resolve(null);
-			}
-		},
-		authenticate: function (username, password, resolve) {
-			if (
-				process.env.NODE_RED_a_USERNAME == username &&
-				process.env.NODE_RED_b_PASSWORD == password
-			) {
-				return resolve({ username: username, permissions: "*" });
-			} else {
-				return resolve(null);
-			}
-		},
-	};
-}
+// Start the runtime
+RED.start();
